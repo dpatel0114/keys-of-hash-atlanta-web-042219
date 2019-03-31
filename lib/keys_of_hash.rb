b36ad41c195b2357d021b2755ccac622754fd529
dpatel0114 <1991dharapatel@gmail.com>
2019-03-31 18:11:28 -0400

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,23 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
+
+
+  def keys_of(animals)
     # code goes here
+    # binding.pry
+    if animals == 'Panama'
+      return ["red-footed tortoise"]
+    elsif animals == 'Madagascar'
+      return ["aye-aye", "tomato frog"]
+    elsif animals == 'Australia'
+      return ["sugar glider", "kangaroo", "koala"]
+
+
+    end
+    return animals[1]
+
   end
+
+
 end
